0c1692374919135f4a0c2999b761604a4452624b
Vittcal Neestackich <neestackich@gmail.com>
2020-07-26 20:53:09 +0300

    start app created only

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Neestackich on 26.07.2020.
+//  Copyright © 2020 Neestackich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
