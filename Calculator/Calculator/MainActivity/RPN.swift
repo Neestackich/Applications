601f0f6e92b1f0a1785f793565a233815d529080
Neestackich <neestackich@gmail.com>
2020-08-01 22:52:12 +0300

    added singly linked list

--- Calculator/Calculator/MainActivity/RPN.swift
+++ Calculator/Calculator/MainActivity/RPN.swift
@@ -10,4 +10,40 @@ import UIKit
 
 class RPN {
     
+    
+    // MARK: operators' types
+    
+    class Symbol {
+        var value: Character!
+        var next: Symbol? = nil
+    }
+    
+    struct Stack {
+        var head: Symbol
+        
+        func push(_ symbol: Character) {
+            if head.value == nil {
+                head.value = symbol
+                head.next = Symbol()
+            } else {
+                var dummy: Symbol = head
+                
+                while dummy.next != nil {
+                    dummy = dummy.next!
+                }
+                
+                dummy.value = symbol
+                dummy.next = Symbol()
+            }
+        }
+        
+        func pop() {
+            
+        }
+    }
+    
+    
+    // MARK: operands' types
+    
+    
 }
