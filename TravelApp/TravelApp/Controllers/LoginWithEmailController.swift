0c3f5f6c0b7cf1099df7729d569790d509516e69
Neestackich <neestackich@gmail.com>
2020-08-08 21:40:26 +0300

    fixed buttons bug, added realb database

--- TravelApp/TravelApp/Controllers/LoginWithEmailController.swift
+++ TravelApp/TravelApp/Controllers/LoginWithEmailController.swift
@@ -8,24 +8,53 @@
 
 import UIKit
 
-class LoginWithEmailController: UIViewController {
+class LoginWithEmailController: UIViewController, UITextFieldDelegate {
     
     
     // MARK: properties
     
     @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var emailUnderline: UIView!
     @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var passwordUnderline: UIView!
-    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var stackWithLabelsAndBtn: UIStackView!
     
-    
+
     
     // MARK: methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let navigationItemHeight = self.navigationController?.navigationBar.frame.height ?? 0.0
+            
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboardByTap)))
+        
         loginButton.layer.cornerRadius = 5
+        emailTextField.keyboardType = .default
+    }
+    
+    @objc func keyboardDidShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let screenHeightWithKeyboard = UIScreen.main.bounds.height - keyboardSize.height - 15
+            
+            if screenHeightWithKeyboard <= forgotPasswordButton.center.y {
+                forgotPasswordButton.center.y -= forgotPasswordButton.center.y - screenHeightWithKeyboard
+                
+                if screenHeightWithKeyboard <= stackWithLabelsAndBtn.center.y + 53 {
+                    stackWithLabelsAndBtn.center.y += stackWithLabelsAndBtn.center.y - screenHeightWithKeyboard
+                }
+            }
+        }
+    }
+    
+    @objc func hideKeyboardByTap() {
+        view.endEditing(true)
     }
 }
