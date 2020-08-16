7f3b77206a7b6353cbdc7ec8d9204cb2eb1d7b2b
Neestackich <neestackich@gmail.com>
2020-08-16 12:07:29 +0300

    added travel list controller, added top bar buttons

--- TravelApp/TravelApp/Controllers/TravelListController.swift
+++ TravelApp/TravelApp/Controllers/TravelListController.swift
@@ -6,43 +6,54 @@
 //  Copyright © 2020 Neestackich. All rights reserved.
 //
 
+
+//добавить кнопку добавления трэвел в реалм
+//просто пихаем объект в реалм по нажатию кнопки и все
+//потом кнопка, которая достает нужный объект(ы) из базы
+
 import UIKit
 
-class TravelListViewController: UIViewController {
+class TravelListViewController: UIViewController, /*UITableViewDataSource*/ UITableViewDelegate {
     
     
     // MARK: Properties
     
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var lastNameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
     @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var travelsTable: UITableView!
     
     var user: User!
-    var nameOpt: String!
-    var email: String!
     
+    let travelAddID: String = "TravelAdd"
     
     // MARK: Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        linking()
+                
+        setup()
     }
     
-    func linking() {
-        nameLabel.text = user?.firstName
-        lastNameLabel.text = user?.lastName
-        emailLabel.text = user?.email
-        nicknameLabel.text = user?.nickName
+    func setup() {
+        //
+        travelsTable.delegate = self
+        //travelsTable.dataSource = self
     }
     
-    static func classInit(user: User) -> TravelListViewController {
-        let travelListController = TravelListViewController()
-        travelListController.user = user
+    @IBAction func addTravelClick(_ sender: Any) {
+        let createTravelVC = storyboard?.instantiateViewController(identifier: travelAddID) as! CreateTravelController
+        createTravelVC.modalPresentationStyle = .fullScreen
+        createTravelVC.userEmail = user?.email
         
+        present(createTravelVC, animated: true)
+    }
+    
+    @IBAction func quitClick(_ sender: Any) {
+        let welcomeScreenVC = storyboard?.instantiateViewController(identifier: "WelcomeScreen") as! WelcomePageController
+        welcomeScreenVC.modalPresentationStyle = .fullScreen
         
-        return travelListController
+        present(welcomeScreenVC, animated: true)
     }
 }
