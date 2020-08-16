ef510acf74f45d24bb37097b7a59c4de76b6e768
Neestackich <neestackich@gmail.com>
2020-08-16 21:33:09 +0300

    added stops list controller

--- TravelApp/TravelApp/Controllers/StopsListController.swift
+++ TravelApp/TravelApp/Controllers/StopsListController.swift
@@ -7,24 +7,68 @@
 //
 
 import UIKit
+import RealmSwift
 
-class StopsListController: UIViewController {
+class StopsListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var stopsTableList: UITableView!
+    
+    var user: User!
+    var travel: Travel!
+    var travelIndex: Int!
+    let addStopVCID: String = "AddStop"
+    let travelListVCID: String = "Travels list"
+    
+    // MARK: Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setup()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setup() {
+        stopsTableList.delegate = self
+        stopsTableList.dataSource = self
+        stopsTableList.separatorStyle = .none
+        stopsTableList.layer.cornerRadius = 15
     }
-    */
-
+    
+    
+    // MARK: -buttons' handlers
+    
+    @IBAction func backToTravelsListControllerClick(_ sender: Any) {
+        let travelsListVC = storyboard?.instantiateViewController(withIdentifier: travelListVCID) as! TravelListViewController
+        travelsListVC.modalPresentationStyle = .fullScreen
+        travelsListVC.user = user
+        
+        present(travelsListVC, animated: true)
+    }
+    
+    @IBAction func addStopClick(_ sender: Any) {
+        let addStopVC = storyboard?.instantiateViewController(withIdentifier: addStopVCID) as! AddStopController
+        addStopVC.modalPresentationStyle = .fullScreen
+        addStopVC.user = user
+        addStopVC.travelIndex = travelIndex
+        
+        present(addStopVC, animated: true)
+    }
+    
+    
+    // MARK: -tableView functions
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return travel.stops.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let stop = travel.stops[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StopCell") as! StopCell
+        cell.cityName.text = stop.stopCityName
+        
+        return cell
+    }
 }
