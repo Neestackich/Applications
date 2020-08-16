c08460a061b52dd75c1a9ec6d352f7ba433f65f4
Neestackich <neestackich@gmail.com>
2020-08-16 21:35:44 +0300

    changed custom travel cell class

--- TravelApp/TravelApp/MainActivity/TravelCell.swift
+++ TravelApp/TravelApp/MainActivity/TravelCell.swift
@@ -9,16 +9,42 @@
 import UIKit
 
 class TravelCell: UITableViewCell {
-
+    
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var countryName: UILabel!
+    @IBOutlet weak var countryDescription: UILabel!
+    
+    
+    // MARK: Methods
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        cellView.layer.cornerRadius = 15
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+        pressAnimatio(objects: cellView, duration: 0.1, resizeDuration: 0.1, x: 0.9, y: 0.9, resizeX: 1, resizeY: 1)
     }
-
+    
+    func pressAnimatio<T: UIView>(objects: T..., duration: TimeInterval, resizeDuration: TimeInterval, x: CGFloat, y: CGFloat, resizeX: CGFloat, resizeY: CGFloat) {
+        UIView.animate(withDuration: duration, animations: {
+            for object in objects {
+                object.transform = CGAffineTransform(scaleX: x, y: y)
+            }
+        }, completion: { _ in
+            UIView.animate(withDuration: resizeDuration, animations: {
+                for object in objects {
+                    object.transform = CGAffineTransform(scaleX: resizeX, y: resizeY)
+                }
+            })
+        })
+    }
 }
