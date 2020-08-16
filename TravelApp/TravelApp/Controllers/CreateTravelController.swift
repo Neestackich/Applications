2e6a4246ff46e1870281dff51b499c29925c3109
Neestackich <neestackich@gmail.com>
2020-08-16 12:05:46 +0300

    added and designed create travel controller

--- TravelApp/TravelApp/Controllers/CreateTravelController.swift
+++ TravelApp/TravelApp/Controllers/CreateTravelController.swift
@@ -7,24 +7,91 @@
 //
 
 import UIKit
+import RealmSwift
 
 class CreateTravelController: UIViewController {
 
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var raitingLabel: UILabel!
+    @IBOutlet weak var nameUnderline: UIView!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var raitingUnderline: UIView!
+    @IBOutlet weak var descriptionUnderline: UIView!
+    @IBOutlet weak var raitingStepper: UIStepper!
+    @IBOutlet weak var descriptionField: UITextField!
+    
+    var travel: Travel!
+    
+    var userEmail: String!
+    
+    
+    // MARK: Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: -buttons' handlers
+    
+    @IBAction func cancelClick(_ sender: Any) {
+        let travelListVC = storyboard?.instantiateViewController(identifier: "Travels list") as! TravelListViewController
+        travelListVC.modalPresentationStyle = .fullScreen
+        
+        present(travelListVC, animated: true)
     }
-    */
-
+    
+    @IBAction func saveClick(_ sender: Any) {
+        if let name = nameField.text, !name.isEmpty, let description = descriptionField.text, !description.isEmpty {
+            var config = Realm.Configuration()
+            
+            if let email = userEmail {
+                config.fileURL = config.fileURL?.deletingLastPathComponent().appendingPathComponent("\(email).realm")
+            }
+            
+            let travelList = try! Realm(configuration: config)
+            
+            //print(travelList.configuration.fileURL)
+            
+            try! travelList.write {
+                travelList.add(Travel(raiting: Int(raitingStepper.value), country: name, travelDescription: description, stops: List<Stop>()))
+            }
+            
+            let travelListVC = storyboard?.instantiateViewController(identifier: "Travels list") as! TravelListViewController
+            travelListVC.modalPresentationStyle = .fullScreen
+            
+            present(travelListVC, animated: true)
+        } else {
+            if (nameField.text?.isEmpty)! {
+                slowedColorChange(objects: nameUnderline, color: .red, duration: 1.3)
+            }
+            
+            if (descriptionField.text?.isEmpty)! {
+                slowedColorChange(objects: descriptionUnderline, color: .red, duration: 1.3)
+            }
+        }
+    }
+    
+    @IBAction func stepperClick(_ sender: Any) {
+        raitingLabel.text = String(raitingStepper.value)
+    }
+    
+    
+    // MARK: -
+    
+    func slowedColorChange<T: UIView>(objects: T..., color: UIColor, duration: TimeInterval) {
+        UIView.animate(withDuration: duration, animations: {
+            for object in objects {
+                if object is UIButton {
+                    object.tintColor = color
+                } else {
+                    object.backgroundColor = color
+                }
+            }
+        })
+    }
 }
