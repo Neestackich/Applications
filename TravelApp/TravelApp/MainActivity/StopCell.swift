01119b84b295ddc8521665ec666eae47d712b4a1
Neestackich <neestackich@gmail.com>
2020-08-16 21:30:58 +0300

    added custom cell and tableView

---
+++ TravelApp/TravelApp/MainActivity/StopCell.swift
@@ -0,0 +1,24 @@
+//
+//  StopCell.swift
+//  TravelApp
+//
+//  Created by Neestackich on 16.08.2020.
+//  Copyright © 2020 Neestackich. All rights reserved.
+//
+
+import UIKit
+
+class StopCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
