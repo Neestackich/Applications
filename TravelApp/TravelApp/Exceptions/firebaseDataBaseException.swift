9cf57dd41bcb4c6d6a63fd7d5a616ec36b66a72d
Neestackich <neestackich@gmail.com>
2020-09-03 01:11:19 +0300

    added firestore pod

---
+++ TravelApp/TravelApp/Exceptions/firebaseDataBaseException.swift
@@ -0,0 +1,13 @@
+//
+//  firebaseDataBaseException.swift
+//  TravelApp
+//
+//  Created by Neestackich on 02.09.2020.
+//  Copyright © 2020 Neestackich. All rights reserved.
+//
+
+import Foundation
+
+enum firebaseDataBaseException: Error {
+    case createAccountError
+}
