7b9a899d7e651efeb3a0fa2155fb1ad07f091efc
Neestackich <neestackich@gmail.com>
2020-09-12 23:33:08 +0300

    deleted activity indicator

---
+++ TravelApp/TravelApp/MainActivity/TravelListControllerDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  TravelListControllerDelegate.swift
+//  TravelApp
+//
+//  Created by Neestackich on 27.08.2020.
+//  Copyright © 2020 Neestackich. All rights reserved.
+//
+
+import UIKit
+
+protocol TravelListControllerDelegate: AnyObject {
+    func update()
+}
---
+++ TravelApp/TravelApp/MainActivity/ViewControllersSwitchManager.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllersSwitchManager.swift
+//  TravelApp
+//
+//  Created by Neestackich on 12.09.2020.
+//  Copyright © 2020 Neestackich. All rights reserved.
+//
+
+import Foundation
